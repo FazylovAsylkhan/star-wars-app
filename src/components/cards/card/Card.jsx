0851f4aff44b0f54dbad8a95e423a7d2999dafc9
Asylkhan Fazylov <asylkhan_fazylov@epam.com>
2022-09-29 20:29:53 +0500

    add: Error Boundary

--- src/components/cards/card/Card.jsx
+++ src/components/cards/card/Card.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable prefer-destructuring */
-import React from "react";
+import React, { useState } from "react";
+import ErrorBoundary from "../../errorBoundary/ErrorBoundary";
 import InfoBox from "../../infoBox";
 import ButtonMore from "../buttomMore";
 import s from "./card.module.scss";
 
-function Card({ results: list, index }) {
+function Card({ results, index }) {
+  const [list, setList] = useState(results);
   return (
-    list && (
-      <div data-testid="card" className={s.card}>
-        <InfoBox results={list} />
-        <ButtonMore idPersonalPage={index} />
-      </div>
+    results && (
+      <ErrorBoundary>
+        <div data-testid="card" className={s.card}>
+          <InfoBox results={list} />
+          <ButtonMore idPersonalPage={index} />
+          <button type="button" onClick={() => setList(null)}>
+            Throw Error
+          </button>
+        </div>
+      </ErrorBoundary>
     )
   );
 }
